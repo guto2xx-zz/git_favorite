d099c4863e0a7ece4018c3758ee0626606d854bf
guto2xx <guto2xx@gmail.com>
2019-10-15 17:40:03 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import main from './pages/Main';
+import User from './pages/User';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      main,
+      User,
+    },
+    {
+      headerLayoutPreset: 'center',
+      heanderBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: `#7159c1`,
+        },
+        headerTintColor: `#FFF`,
+      },
+    }
+  )
+);
+
+export default Routes;
